a6a246b77c71a0ae0ad4d5f70d12f90de0deb4f6
Meklodn2 <amaxmudbekov@mail.ru>
2025-03-06 11:32:06 +0500

    project satrted

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from users.views import SignUpView, SignInView, user_logout, ProfileView,ProfileEditView, UserPasswordChangeView
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', SignInView.as_view(), name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('profile/', ProfileView.as_view(),name='profile'),
+    path('profile/edit/', ProfileEditView.as_view(),name='profile-edit'),
+      path('profile/change_password/', UserPasswordChangeView.as_view(),name='change-password'),
+]
---
+++ users/views.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.views.generic import CreateView, DetailView, UpdateView
+from .models import CustomUser
+from .forms import UserSignUpForm, UserSignInForm, UserUpdateForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+# Create your views here.
+
+class SignUpView(CreateView):
+    model = CustomUser
+    form_class = UserSignUpForm
+    template_name = 'main/auth/sign_up.html'
+    success_url = reverse_lazy('login')
+    def form_valid(self, form):
+        user = form.save()
+        login (self.request, user)
+        return super().form_valid(form)
+
+class SignInView(LoginView):
+    form_class = UserSignInForm
+    template_name = 'main/auth/sign_in.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = CustomUser
+    template_name = 'main/profile.html'
+    context_object_name = 'user_profile'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class ProfileEditView(LoginRequiredMixin,UpdateView):
+    model = CustomUser
+    form_class = UserUpdateForm
+    template_name = 'main/profile_edit.html'
+    context_object_name = 'user_profile'
+    success_url = reverse_lazy('profile')
+    def get_object(self,queyset=None):
+        return self.request.user
+
+class UserPasswordChangeView(PasswordChangeView):
+    template_name = 'main/auth/change_password.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        messages.success(self.request, '👌 your password has been changed succesfully')
+        return super().form_valid(form)
